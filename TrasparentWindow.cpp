44f8afc35db9026b16035634fe784faeb8172582
v-guhu <v-guhu@expedia.com>
2013-12-23 02:27:43 -0800

    display the desktop in transparent window

--- TrasparentWindow.cpp
+++ TrasparentWindow.cpp
@@ -16,34 +16,80 @@ BOOL TrasparentWindow::Create()
 
 	// No border window 
 	return CBaseWindow::Create(~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZE | WS_MAXIMIZE | WS_SYSMENU | WS_HSCROLL | WS_VSCROLL), &rect) ;
+	//return CBaseWindow::Create( WS_OVERLAPPEDWINDOW | WS_VISIBLE, &rect) ;
+}
+
+void TrasparentWindow::SetWindowTransparent (HWND hwnd) 
+{
+	// Set WS_EX_LAYERED on this window 
+	SetWindowLong (hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED /*| WS_EX_TRANSPARENT*/);
+	// Make this window 10% alpha
+	SetLayeredWindowAttributes (hwnd, 0, (255 * 10) / 100, LWA_ALPHA) ;
+}
+
+HBITMAP TrasparentWindow::CreateDesktopBitmap ()
+{		
+    int nWidth = GetSystemMetrics (SM_CXSCREEN) ;
+    int nHeight = GetSystemMetrics (SM_CYSCREEN) ;
+
+	HWND hDesktopWnd =  GetDesktopWindow () ;
+    HDC hDesktopDC = GetDC (hDesktopWnd) ;    
+	HDC hBmpFileDC = CreateCompatibleDC (hDesktopDC) ;
+
+    hBitmap = CreateCompatibleBitmap (hDesktopDC,nWidth,nHeight) ;
+    HBITMAP hOldBitmap = (HBITMAP) SelectObject (hBmpFileDC, hBitmap) ;
+    BitBlt (hBmpFileDC, 0, 0, nWidth, nHeight, hDesktopDC, 0, 0, SRCCOPY|CAPTUREBLT) ;
+    SelectObject (hBmpFileDC,hOldBitmap) ; 
+	 
+	DeleteDC (hDesktopDC) ;
+	DeleteObject (hDesktopWnd) ;
+	DeleteObject (hOldBitmap) ;
+	DeleteDC (hBmpFileDC) ;
+	return hBitmap ;
+}
+
+void TrasparentWindow::OnCreate (HWND hwnd)
+{	
+	CreateDesktopBitmap ();	
+    //Todo: m_hwnd != hwnd?
+	//SetWindowTransparent (hwnd) ;
+}
+
+void TrasparentWindow::OnPaint ()
+{
+	if (hBitmap)
+	{
+		BITMAP             bm ;
+		RECT               rcClient;
+		PAINTSTRUCT        ps;	
+		HDC                hdcClient, hdcMem;
+		
+		GetClientRect (m_hwnd, &rcClient) ;
+		hdcClient = BeginPaint(m_hwnd, &ps);
+		GetClientRect(m_hwnd, &rcClient) ;
+		hdcMem = CreateCompatibleDC (hdcClient) ;
+		SelectObject (hdcMem, hBitmap) ;
+		GetObject (hBitmap, sizeof (BITMAP), (PSTR) &bm) ;
+		SetStretchBltMode (hdcClient, COLORONCOLOR) ;
+		StretchBlt (hdcClient, 0, 0, rcClient.right, rcClient.bottom,
+                   hdcMem, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY) ;	
+		
+		DeleteDC (hdcMem) ;
+		EndPaint (m_hwnd, &ps) ;
+	}	
 }
 
 LRESULT CALLBACK TrasparentWindow::WinMsgHandler (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	HBITMAP hBitmap = NULL ;
+
 	switch (uMsg)
 	{
-	case WM_CREATE:
-		 {
-			 // Set WS_EX_LAYERED on this window 
-			 SetWindowLong (hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED /*| WS_EX_TRANSPARENT*/);
-			 // Make this window 70% alpha
-			 SetLayeredWindowAttributes (hwnd, 0, (255 * 1) / 100, LWA_ALPHA) ;
-		 }			
-		//OnCreate();
+	case WM_CREATE:			
+		OnCreate(hwnd) ;
 		break ;		
-	case WM_PAINT:
-		{
-			RECT               rcClient;
-			PAINTSTRUCT        ps;	
-			HDC                hdcClient, hdcMem;	
-			
-			hdcClient = BeginPaint(hwnd, &ps);
-			GetClientRect(hwnd, &rcClient) ;
-			hdcMem = CreateCompatibleDC (hdcClient) ;	
-			TextOut(hdcClient, 500, 500, TEXT("No Border window"), 30);
-			EndPaint (hwnd, &ps);
-		}
-		//OnPaint () ;
+	case WM_PAINT:		
+		OnPaint () ;
 		break ;
 	case WM_LBUTTONDOWN:		
 		//OnLButtonDown (wParam, lParam) ;
--- TrasparentWindow.h
+++ TrasparentWindow.h
@@ -6,14 +6,20 @@ class TrasparentWindow : public CBaseWindow
 public:
 	TrasparentWindow::TrasparentWindow (HINSTANCE hInst, CONST WNDCLASSEX* wcx = NULL) : CBaseWindow(hInst, wcx)
 	{
+		hBitmap = NULL;
 		SetWindowTitle ("Luna") ;
 	} ;
 
 	~TrasparentWindow(void) ;
 
 	BOOL Create() ;
+	void SetWindowTransparent (HWND hwnd) ;
+	HBITMAP CreateDesktopBitmap () ;
+	void OnCreate (HWND hwnd) ;
+	void OnPaint () ;
 
-protected:	
+protected:
+	HBITMAP	hBitmap ;
 	LRESULT CALLBACK WinMsgHandler (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) ;
 };
 
